ac9a5cc8a796ba252644bf49fc5fd0fb86d64309
Stewart James <>
2024-11-18 19:25:02 +1100

    fix: use correct directory for ansible hash

--- run_onchange_macos-setup.sh.tmpl.sh
+++ run_onchange_macos-setup.sh.tmpl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Macos-setup hash: {{tar -cf - dot_local/share/macos-setup | sha1sum }}
+# Macos-setup hash: {{tar -cf - ".local/share/macos-setup" | sha1sum }}
 echo "Updating MacOS Configurqation."
 ansible-playbook $HOME/.local/share/macos-setup/main.yml --ask-become-pass
