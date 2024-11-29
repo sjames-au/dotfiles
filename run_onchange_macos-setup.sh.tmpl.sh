e5139f337779dae5999243237b1449cb41544251
Stewart James <>
2024-11-30 08:40:52 +1100

    fix: identify if macos-setup has changed

--- run_onchange_macos-setup.sh.tmpl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Macos-setup hash: {{tar -cf - "~/.local/share/macos-setup" | sha1sum }}
-echo "Updating MacOS Configurqation."
-ansible-playbook $HOME/.local/share/macos-setup/main.yml --ask-become-pass
